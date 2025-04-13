87c3f6a1f1189fef427a8360f22bc68389ed2aec
Keokeditse Ndala <g23n3545@campus.ru.ac.za>
2025-04-13 14:35:31 +0200

    Done with Task 5 - by Kay

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -1,7 +1,19 @@
 package sdp.prac2;
-
+import java.lang.Comparable;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    // public SimpleFunctions() {}
+
+    public static <T extends Comparable<T>> boolean Task5 (List<T> lst) {
+        // A loop that will iterate through the list
+        for (int i =0; i < lst.size() - 1; i++) {
+            // Checking if the item in the current index is smaller than the item at index + 1...
+            // If not, return false
+            if (lst.get(i).compareTo(lst.get(i + 1)) > 0)
+                return false;
+        }
+        // if the list is sorted, it will return true
+        return true;
+    }
 }
