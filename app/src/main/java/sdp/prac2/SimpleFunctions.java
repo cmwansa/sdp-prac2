a216ce7a97c852544d9427bc69f22488599a50be
ChichiMwansa <joyhasjoy04@gmail.com>
2025-04-15 00:23:11 +0200

    Created a task 1 method

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,15 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+
+    public int task1 (int [] a, int [] b){
+    int sum = 0;
+    for (int i = 0; i < b.length; i++) {
+        int index = b[i];
+        if (index < a.length) {
+        sum += a[index];
+        }
+    } 
+    return sum;
+    }
 }
