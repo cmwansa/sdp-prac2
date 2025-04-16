a2f39a47d05d1ecf5e322a64e2a5267ee88e6f8a
Otha <othandwayomyona04@gmail.com>
2025-04-16 17:11:36 +0200

    Complete Task 4

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -65,7 +65,21 @@ public class SimpleFunctions {
         return (check == true && count == 0);   //indicates that a string has brackets AND that they are matched
     }
 
+    //Task 4
+    public static List<Integer> Task4(List<Integer> a, List<Integer> b) {
+        if (a.size() != b.size()) { // Check if lists are the same size 
+            return null;    //If not, return null
+        }
 
+        List<Integer> result = new ArrayList<>(); // List to store the result of the multiplications
+
+        for (int i = 0; i < a.size(); i++) {
+            int product = a.get(i) * b.get(b.size() - 1 - i); // Multiply a[i] with the element from the end of b
+            result.add(product);    // Add product to the result list
+        }
+
+        return result;
+    }
     
     public static <T extends Comparable<T>> boolean Task5 (List<T> lst) {
         // A loop that will iterate through the list
